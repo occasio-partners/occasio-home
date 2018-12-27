5cf1355e582c614d7691df61a6512edbd2be4551
Nathaniel Hill <nata@goguna.com>
2018-12-27 14:57:42 -0600

    don't leak secret key, not needed on frontend

--- www/next.config.js
+++ www/next.config.js
@@ -22,7 +22,6 @@ module.exports = withPlugins(plugins,
     publicRuntimeConfig: {
       NODE_ENV: process.env.NODE_ENV,
       ANALYTICS_TOKEN: process.env.ANALYTICS_TOKEN,
-      RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
       RECAPTCHA_PUBLIC_KEY: process.env.RECAPTCHA_PUBLIC_KEY
     },
     webpack: config => {
