e6fc0ad8bb379cd9c1add828e7693109eccb1853
Nathaniel Hill <nata@goguna.com>
2018-12-27 14:39:50 -0600

    add dummy careers page

---
+++ www/pages/careers.js
@@ -0,0 +1,10 @@
+import FadingComponent from 'components/FadingComponent'
+
+export default () => <FadingComponent>
+  <div className='box3-container'>
+    <div className='box3-bg' />
+    <div className='box3'>
+      <h2>Careers</h2>
+    </div>
+  </div>
+</FadingComponent>
