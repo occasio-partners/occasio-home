b327d3e1843640cff89ebc45c6c5687d5b593bfe
Nathaniel Hill <nata@goguna.com>
2018-12-27 20:27:17 -0600

    cleanup, handle props properly

--- www/components/ProgressButton.js
+++ www/components/ProgressButton.js
@@ -1,22 +1,18 @@
 import React from 'react'
 
-export default () => {
-  const {
-    className,
-    inProgress,
-    isDone,
-    inProgressText,
-    isDoneText,
-    children,
-    ...props
-  } = this.props
-
-  return <button className={`progress-button ${className}`} {...props}>
-    {isDone
-      ? <div>{isDoneText && <span>{isDoneText}</span>}</div>
-      : inProgress
-        ? <div>{inProgressText && <span>{inProgressText}</span>}</div>
-        : (children)
-    }
-  </button>
-}
+export default ({
+  className,
+  inProgress,
+  isDone,
+  inProgressText,
+  isDoneText,
+  children,
+  ...props
+}) => <button className={`progress-button ${className}`} {...props}>
+  {isDone
+    ? <div>{isDoneText && <span>{isDoneText}</span>}</div>
+    : inProgress
+      ? <div>{inProgressText && <span>{inProgressText}</span>}</div>
+      : (children)
+  }
+</button>
