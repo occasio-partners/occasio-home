80e0d976b824997f23ddf161e1f2db67e98f45cf
Nathaniel Hill <nata@goguna.com>
2019-01-03 14:05:02 -0600

    simplify components

--- www/components/FadingBox.js
+++ www/components/FadingBox.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import FadingComponent from 'components/FadingComponent'
-import ContentBox from 'components/ContentBox'
 
 export default ({ children }) =>
   <FadingComponent>
-    <ContentBox>
-      {children}
-    </ContentBox>
+    <div>
+      <div className='content'>
+        {children}
+      </div>
+      <style jsx>{`
+        div {
+          margin: 0 auto;
+          max-width: 50em;
+        }
+        .content {
+          background: rgba(34,34,34,0.75);
+          padding: 1em;
+        }
+        @media screen and (min-width: 600px) {
+          .content {
+            padding: 2em;
+            margin: 4em;
+          }
+        }
+      `}</style>
+    </div>
   </FadingComponent>
