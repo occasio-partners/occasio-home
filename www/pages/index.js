1d4e857df22a5455fdd996f72e176fe74a79301a
Nathaniel Hill <nata@goguna.com>
2019-01-21 22:58:08 -0500

    update text per email

--- www/pages/index.js
+++ www/pages/index.js
@@ -4,6 +4,6 @@ export default () =>
   <FadingBox>
     <h1>Welcome to Occasio Partners, LLC</h1>
     <p>
-      Occasio Partners, LLC is an alternative investment firm that focuses on generating low-risk returns by utilizing differing markets and various time frames to deliver consistent results. For a prospectus, monthly letter update, minimum investment, fee structure, or other inquires. Please email info@occasio-partners.com
+      Occasio Partners, LLC is a San-Francisco based alternative investment fund.  Occasio, focuses on multiple markets and various time frames including equities, currencies, commodities and fixed-income.
     </p>
   </FadingBox>
