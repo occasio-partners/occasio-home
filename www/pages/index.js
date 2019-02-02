e649823bff83dda836927ca12152c13287626dbf
Nathaniel Hill <nata@goguna.com>
2019-02-01 20:56:21 -0500

    remove LLC from title

--- www/pages/index.js
+++ www/pages/index.js
@@ -2,7 +2,7 @@ import FadingBox from 'components/FadingBox'
 
 export default () =>
   <FadingBox>
-    <h1>Welcome to Occasio Partners, LLC</h1>
+    <h1>Welcome to Occasio Partners</h1>
     <p>
       Occasio Partners, LLC is a San-Francisco based alternative investment fund.  Occasio, focuses on multiple markets and various time frames including equities, currencies, commodities and fixed-income.
     </p>
